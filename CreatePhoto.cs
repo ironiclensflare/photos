fcb86a4cce662a175944c582d7f4184d72a08217
Stuart Carruthers <stu@ironiclensflare.com>
2018-07-10 09:30:09 +0100

    CreatePhoto now accepts photo

--- CreatePhoto.cs
+++ CreatePhoto.cs
@@ -6,25 +6,18 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Photos.Models;
 
 namespace Photos
 {
     public static class CreatePhoto
     {
         [FunctionName("CreatePhoto")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var requestBody = new StreamReader(req.Body).ReadToEnd();
+            var photo = JsonConvert.DeserializeObject<Photo>(requestBody);
+            return new OkObjectResult(photo);
         }
     }
 }
